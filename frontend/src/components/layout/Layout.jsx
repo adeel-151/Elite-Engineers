638ad3b95af56cebbeb425336a019f6e6f2a3b4a
adeel-151 <adeelkhattak223@gmail.com>
2026-07-23 16:20:03 +0500

    fix: prevent horizontal scroll on mobile by adding overflow-x hidden to html, body and layout

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -6,7 +6,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <Navbar />
       <main className="flex-grow pt-0">
         <Outlet />
